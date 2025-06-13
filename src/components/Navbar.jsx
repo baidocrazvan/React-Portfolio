68de3c3c9334b2eeae4c32263ca2cf8ab4afcc5a
Baidoc Razvan <baidocrazvan@gmail.com>
2025-06-13 15:37:50 +0300

    feat: Add completed Navbar component with full functionality

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,98 @@
+import { cn } from "../lib/utils";
+import { useEffect, useState } from "react";
+import { Menu, X } from "lucide-react";
+
+// Header sections
+const navItems = [
+  { name: "Home", href: "#hero" },
+  { name: "About", href: "#about" },
+  { name: "Skills", href: "#skills" },
+  { name: "Projects", href: "#projects" },
+  { name: "Contact", href: "#contact" },
+];
+
+export const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState(false);
+  {
+    /* Track of mobile menu is open */
+  }
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      // Set to true if the distance of scroll is greater than the size of navbar
+      setIsScrolled(window.screenY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+  return (
+    <nav
+      className={cn(
+        "fixed w-full z-40 transition-all duration-300",
+        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+      )}
+    >
+      <div className="container flex items-center justify-between">
+        {/* Navbar Logo */}
+        <a
+          className="text-xl font-bold text-primary flex items-center"
+          href="#hero"
+        >
+          <span className="relative z-10">
+            <span className="text-glow text-foreground"> Baidoc Razvan's </span>{" "}
+            Portfolio
+          </span>
+        </a>
+
+        {/* desktop nav */}
+        <div className="hidden md:flex space-x-8">
+          {/* keep hidden for small screens*/}
+          {navItems.map((item, key) => (
+            <a
+              key={key}
+              href={item.href}
+              className="text-foreground/80 hover:text-primary transition-colors duration-300"
+            >
+              {item.name}
+            </a>
+          ))}
+        </div>
+
+        {/* vertical mobile nav */}
+        <button
+          onClick={() => setIsMenuOpen((prevIsMenuOpen) => !prevIsMenuOpen)}
+          className="md:hidden p-2 text-foreground z-50"
+          aria-label={isMenuOpen ? "Close Menu" : "Open Menu"}
+        >
+          {isMenuOpen ? <X size={24} /> : <Menu size={24} />}
+        </button>
+
+        <div
+          className={cn(
+            "fixed inset-0 bg-background/95 backdrop-blur-md z-40 flex flex-col items-center justify-center",
+            "transition-all duration-300 md:hidden",
+            isMenuOpen
+              ? "opacity-100 pointer-events-auto"
+              : "opacity-0 pointer-events-none"
+          )}
+        >
+          <div className="flex flex-col space-y-8 text-xl">
+            {navItems.map((item, key) => (
+              <a
+                key={key}
+                href={item.href}
+                className="text-foreground/80 hover:text-primary transition-colors duration-300"
+                // Set menu to closed to exit navbar when item is pressed
+                onClick={() => setIsMenuOpen(false)}
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};

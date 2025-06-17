144dd11aab89d2d1ec6641266f717ec52eefc2b7
baidocrazvan <baidocrazvan@gmail.com>
2025-06-17 15:22:19 +0300

    fix: Fix navbar broken if opened while scrolling on mobile

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -42,7 +42,7 @@ export const Navbar = () => {
     <nav
       className={cn(
         "fixed w-full z-40 transition-all duration-300",
-        isScrolled ? "py-3 bg-background/80 backdrop-blur-md shadow-xs" : "py-5"
+        isScrolled ? "py-3 bg-background/95 shadow-xs" : "py-5"
       )}
     >
       <div className="container flex items-center justify-between">
